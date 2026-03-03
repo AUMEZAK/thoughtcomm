9e1275ef5cb5537dcd8ccdd30549aa7cf4d7fbaa
AUMEZAK <umeatsu4@gmail.com>
2026-03-03 16:07:43 +0900

    Fix total_mem -> total_memory in utils/memory.py

--- utils/memory.py
+++ utils/memory.py
@@ -23,7 +23,7 @@ def get_memory_stats():
 
     allocated = torch.cuda.memory_allocated() / 1024**2
     reserved = torch.cuda.memory_reserved() / 1024**2
-    total = torch.cuda.get_device_properties(0).total_mem / 1024**2
+    total = torch.cuda.get_device_properties(0).total_memory / 1024**2
     free = total - reserved
 
     return {
